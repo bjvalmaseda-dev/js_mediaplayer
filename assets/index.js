3a898a433a9da39cc98c1e876e6727399ea8f2c8
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-02 14:48:49 -0400

    :sparkles: [ADD] AutoPlay plugin

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,14 @@
 import MediaPlayer from "./MediaPlayer.js"
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, 
+    plugins: [
+       new AutoPlay()
+    ] 
+});
 
 const button = document.querySelector("button");
+const muted= document.getElementById('mutedButton');
 button.onclick = () => player.togglePlay();
+muted.onclick=()=>player.toggleMute();
