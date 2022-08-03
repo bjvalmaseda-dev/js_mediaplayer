5b696da48768b9ddb00f25a434e8801e88151d30
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-02 21:25:03 -0400

    :recycle: [ADD] get and setters

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,11 @@ import MediaPlayer from "./MediaPlayer.js"
 import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video, 
-    plugins: [
-       new AutoPlay()
-    ] 
+const player = new MediaPlayer(
+    { el: video, 
+        plugins: [
+      // new AutoPlay()
+        ] 
 });
 
 const button = document.querySelector("button");
